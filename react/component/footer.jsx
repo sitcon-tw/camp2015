14e110f111eb1e9c21fab0a21415eaf1da6ff27b
Sunset <s2821d3721@gmail.com>
2015-05-04 21:28:55 +0800

    add footer's link

--- react/component/footer.jsx
+++ react/component/footer.jsx
@@ -6,6 +6,12 @@ var Footer = React.createClass({
 		return (
 			<div className="Footer">
 				<footer>{"學生計算機年會籌備團隊"}</footer>
+				<div className="followUs">
+					<a target="_blank" href="https://www.facebook.com/SITCONCamp">Facebook</a>
+					<a target="_blank" href="https://www.flickr.com/photos/sitcon/sets/">Flickr</a>
+					<a target="_blank" href="https://www.youtube.com/channel/UCMXFGmpqKAowZMbvz2O2aEw">Youtube</a>
+					<a target="_blank" href="http://sitcon.org/2015/#/">SITCON 2015</a>
+				</div>
 			</div>
 		);
 	}
