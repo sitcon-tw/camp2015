1c208c5517f33432e01a4a514666c89a22ac5386
Sunset <s2821d3721@gmail.com>
2015-05-04 22:46:02 +0800

    update course

--- react/component/coursePage.jsx
+++ react/component/coursePage.jsx
@@ -8,41 +8,52 @@ var timetableContent = [
 		{t:["day4"]}
 	],
 	[
-		{t:["08:00","~","08:30"]},
-		{t:[" "],type:"easy"},
-		{t:["勇者，登入！","每日晨間任務"],c:3,type:"activity"}
+		{t:["08:30","~","09:30"]},
+		{t:["開始報到"],type:"easy"},
+		{t:["社群精神"],type:"course"},
+		{t:["WEB"],type:"course",r:3},
+		{t:["視界咖啡館"],type:"activity",r:3}
 	],
 	[
-		{t:["08:30","~","09:00"]},
-		{t:["角色，創建！","報到"],type:"activity"},
-		{t:["早餐"],c:3,type:"easy"}
+		{t:["09:30","~","10:30"]},
+		{t:["開幕式"],type:"easy"},
+		{t:["SITCON心路歷程"],type:"course"}
 	],
 	[
-		{t:["09:00","~","09:30"]},
-		{t:["開幕"]},
-		{t:["Open Source 新手上路"],r:2,l:" ",type:"course"},
-		{t:["輕鬆學會網頁前端","網頁教學","聽風"],r:5,l:" ",type:"course"},
-		{t:["出征，預備！","蓄勢待發"],r:2,type:"activity"}
+		{t:["10:30","~","12:00"]},
+		{t:["小隊破冰"],type:"activity"},
+		{t:["贊助議程"],type:"course"}
 	],
 	[
-		{t:["09:30","~","10:00"]},
-		{t:["勇者，組隊！","認識夥伴"],type:"activity"},
+		{t:["12:00","~","13:30"]},
+		{t:["小惡魔信箱","午餐"],c:3,type:"easy"},
+		{t:["午餐"],type:"easy"}
 	],
 	[
-		{t:["10:00","~","11:00"]},
-		{t:["我把源碼都放在那了！","Kirby Wu"],l:" ",type:"course"},
-		{t:["資訊自學能力培養 座談會","Legist 強","MouseMs","Denny Huang","RSChiang","Richard Lee","陳恭老師"],r:3,l:" ",type:"activity"},
-		{t:["e-Tutor 線上協同學習平台"],type:"course"}
+		{t:["13:30","~","15:00"]},
+		{t:["Git"],type:"course",r:2},
+		{t:["Web"],type:"course",r:2},
+		{t:["Security（資安）"],type:"course",r:2},
+		{t:["社群闖關"],type:"activity"}
 	],
 	[
-		{t:["11:00","~","11:10"]},
-		{t:["休息"],type:"easy"},
-		{t:["休息"],type:"easy"}
+		{t:["15:00","~","17:00"]},
+		{t:["hackathon發表","故事時間"],type:"activity"}
 	],
 	[
-		{t:["11:10","~","12:00"]},
-		{t:["我把源碼都放在那了！","Kirby Wu"],l:" ",type:"course"},
-		{t:["創新世界的力量","洞悉大數據密碼","朱皇韋"],type:"course"}
+		{t:["17:00","~","18:30"]},
+		{t:["小惡魔信箱","晚餐"],c:3,type:"easy"},
+		{t:["閉幕","賦歸"],type:"easy"}
+	],
+	[
+		{t:["18:00","~","21:00"]},
+		{t:["夜教"],type:"activity",c:2},
+		{t:["Hackathon"],type:"activity"},
+		{t:[" "],type:"easy",r:2}
+	],
+	[
+		{t:["21:00","~","22:00"]},
+		{t:["小隊時間","宵夜"],type:"easy",c:3}
 	]
 ];
 
@@ -50,12 +61,14 @@ var React = require('react');
 var Timetable = require('../partial/timetable.jsx');
 
 var CoursePage = React.createClass({
+	/*
+				<div className="comingSoon">Coming Soon...</div>
+	*/
 	render: function(){
 		return (
 			<div className="coursePage pageContainer">
 				<h2>課 程</h2>
 				<Timetable content={timetableContent} />
-				<div className="comingSoon">Coming Soon...</div>
 			</div>
 		);
 	}
